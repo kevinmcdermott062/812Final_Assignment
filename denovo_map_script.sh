8cacef524ac0a8458b17982a58d3421657f30242
Kevin McDermott <kevin.mcdermott@oahpp.ca>
2018-02-27 11:06:33 -0500

    added two bash scripts and PCA analysis markdown

---
+++ denovo_map_script.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+#SBATCH -J denovo_map
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH --mem=40g
+#SBATCH -t 14-0:0:0
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=kevin.mcdermott@oahpp.ca
+#SBATCH -o output_denovo_map%j.o
+
+module load stacks/1.46
+
+denovo_map.pl \
+-O ./popmap.txt -o ./denovo_assembly  -m 3 -M 2 -b 1 -S -T 4 \
+-s ./1007.fq  \
+-s ./100.fq \
+-s ./14.fq \
+-s ./15.fq \
+-s ./16.fq \
+-s ./17.fq \
+-s ./18.fq \
+-s ./19.fq \
+-s ./25.fq \
+-s ./26.fq \
+-s ./27.fq \
+-s ./28.fq \
+-s ./30.fq \
+-s ./31.fq \
+-s ./32.fq \
+-s ./33.fq \
+-s ./34.fq \
+-s ./35.fq \
+-s ./36.fq \
+-s ./37.fq \
+-s ./38.fq \
+-s ./39.fq \
+-s ./40.fq \
+-s ./41.fq \
+-s ./42.fq \
+-s ./43.fq \
+-s ./44.fq \
+-s ./45.fq \
+-s ./46.fq \
+-s ./47.fq \
+-s ./48.fq \
+-s ./49.fq \
+-s ./60.fq \
+-s ./61.fq \
+-s ./70.fq \
+-s ./71.fq \
+-s ./723.fq \
+-s ./724.fq \
+-s ./725.fq \
+-s ./726.fq \
+-s ./727.fq \
+-s ./728.fq \
+-s ./729.fq \
+-s ./72.fq \
+-s ./730.fq \
+-s ./731.fq \
+-s ./732.fq \
+-s ./73.fq \
+-s ./741.fq \
+-s ./742.fq \
+-s ./743.fq \
+-s ./753.fq \
+-s ./754.fq \
+-s ./755.fq \
+-s ./756.fq \
+-s ./757.fq \
+-s ./758.fq \
+-s ./75.fq \
+-s ./760.fq \
+-s ./761.fq \
+-s ./762.fq \
+-s ./763.fq \
+-s ./76.fq \
+-s ./776.fq \
+-s ./777.fq \
+-s ./77.fq \
+-s ./78.fq \
+-s ./792.fq \
+-s ./793.fq \
+-s ./794.fq \
+-s ./795.fq \
+-s ./79.fq \
+-s ./80.fq \
+-s ./810.fq \
+-s ./81.fq \
+-s ./82.fq \
+-s ./833.fq \
+-s ./84.fq \
+-s ./850.fq \
+-s ./851.fq \
+-s ./85.fq \
+-s ./868.fq \
+-s ./869.fq \
+-s ./86.fq \
+-s ./87.fq \
+-s ./88.fq \
+-s ./89.fq \
+-s ./90.fq \
+-s ./91.fq \
+-s ./92.fq \
+-s ./93.fq \
+-s ./94.fq \
+-s ./95.fq \
+-s ./96.fq \
+-s ./97.fq \
+-s ./98.fq \
+-s ./99.fq  \
+-s ./L_627.fq 
+
