57495af8f39215562269fc67f8e9beb1cd341fc2
PeiwenLi <36014340+PeiwenLi@users.noreply.github.com>
2018-04-04 18:06:47 -0400

    Create structure.sh

---
+++ STRUCTURE/structure.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH --mem=5g
+#SBATCH -t 14-0:0:0
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=15pl16@queensu.ca
+module load structure
+structure -m mainparams_k1 -e extraparams_nolocprior -K 1
+structure -m mainparams_k2 -e extraparams_nolocprior -K 2
